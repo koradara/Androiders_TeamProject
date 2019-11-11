b939e326964591ca8ef420ed1c502280f2932024
Haonan <zhanhaon@sheridancollege.ca>
2019-11-11 15:28:17 -0500

    Branch Test Terry

--- app/src/main/java/com/example/parkandgoapp/MainActivity.java
+++ app/src/main/java/com/example/parkandgoapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import android.util.Log;
 import android.view.View;
 
 import androidx.navigation.NavController;
@@ -51,6 +52,8 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        Log.e("MainActivity", "Test branch");
     }
 
     @Override
